6a0771d7904b042a012e91a676c761a1d0bf2f4a
kyledetella <kdetella@gmail.com>
2015-03-31 20:39:38 -0500

    Update build to use browserify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,30 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
+var browserify = require('browserify');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var jshint = require('gulp-jshint');
 var reporter = require('jshint-stylish');
 var size = require('gulp-size');
 var bump = require('gulp-bump');
+var source = require('vinyl-source-stream');
 
 var SRC = './src/viewport-state-manager.js';
 
-/* Build source */
 gulp.task('compile', function () {
-  gulp.src(SRC)
-  .pipe(browserify({
-    standalone: 'ViewportStateManager'
-  }))
-  .pipe(size())
+  return browserify(SRC, {standalone: 'ViewportStateManager'})
+  .bundle()
+  .pipe(source('viewport-state-manager.js'))
   .pipe(gulp.dest('./dist'))
 });
 
 gulp.task('build:src', ['test', 'compile'], function () {
-  gulp.src('dist/viewport-state-manager.js')
-  .pipe(uglify())
-  .pipe(rename('viewport-state-manager.min.js'))
-  .pipe(size())
-  .pipe(gulp.dest('./dist'));
+  return gulp.src('dist/viewport-state-manager.js')
+    .pipe(size({showFiles: true}))
+    .pipe(uglify())
+    .pipe(rename('viewport-state-manager.min.js'))
+    .pipe(size({showFiles: true}))
+    .pipe(size({gzip: true, showFiles: true}))
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('test', function () {
